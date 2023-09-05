3261bc8f8fcb58128a5a6e629ace30feddf5d365
olaniyi victor <victorolaniyi22@gmail.com>
2023-09-05 15:09:27 +0100

    loop studios landing page

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,47 +2,62 @@
 import Button from './Button';
 import { motion } from 'framer-motion';
 import Gallerys from './Gallerys';
-
+import deepEarthDesktop from "../src/assets/images/desktop/image-deep-earth.jpg";
+import mobiledeepEarth from  "../src/assets/images/mobile/image-deep-earth.jpg";
+import arcadeDesktop from "../src/assets/images/desktop/image-night-arcade.jpg";
+import mobilearcade from "../src/assets/images/mobile/image-night-arcade.jpg";
+import soccerteamDesktop from "../src/assets/images/desktop/image-soccer-team.jpg";
+import soccerteammobile from "../src/assets/images/mobile/image-soccer-team.jpg";
+import gridDestop from "../src/assets/images/desktop/image-grid.jpg";
+import gridmobile from "../src/assets/images/mobile/image-grid.jpg";
+import fromaboveDesktop  from  "../src/assets/images/desktop/image-from-above.jpg";
+import fromabovemobile from "../src/assets/images/mobile/image-from-above.jpg";
+import pocketborealisDesktop  from "../src/assets/images/desktop/image-pocket-borealis.jpg";
+import pocketborealismobile from "../src/assets/images/mobile/image-pocket-borealis.jpg";
+import curiosityDesktop from "../src/assets/images/desktop/image-curiosity.jpg";
+import curiositymobile from "../src/assets/images/mobile/image-curiosity.jpg";
+import fisheyeDesktop from "../src/assets/images/desktop/image-fisheye.jpg";
+import fisheyemobile from"../src/assets/images/mobile/image-fisheye.jpg";
 const InitialImage = [
   {
-    desktop: "../src/assets/images/desktop/image-deep-earth.jpg",
-    mobile: "../src/assets/images/mobile/image-deep-earth.jpg",
+    desktop: deepEarthDesktop,
+    mobile: mobiledeepEarth,
     text: 'DEEP',
     texts: 'EARTH',
   },
-  {desktop:"../src/assets/images/desktop/image-night-arcade.jpg",
-  mobile:"../src/assets/images/mobile/image-night-arcade.jpg",
+  {desktop:arcadeDesktop,
+  mobile:mobilearcade,
   text:'NIGHT',
   texts:'ARCADE'
 },
-  {desktop:"../src/assets/images/desktop/image-soccer-team.jpg",
-  mobile:"../src/assets/images/mobile/image-soccer-team.jpg",
+  {desktop:soccerteamDesktop,
+  mobile:soccerteammobile,
   text:'SOCCER',
   texts:'TEAM VR',
 },
-  {desktop:"../src/assets/images/desktop/image-grid.jpg",
-  mobile:"../src/assets/images/mobile/image-grid.jpg",
+  {desktop:gridDestop,
+  mobile:gridmobile,
   text:'THE',
   texts:'GRID',
 },
- {desktop: "../src/assets/images/desktop/image-from-above.jpg",
- mobile:"../src/assets/images/mobile/image-from-above.jpg",
+ {desktop: fromaboveDesktop,
+ mobile:fromabovemobile,
  text:'FROM UP',
  texts:'ABOVE VR',
 },
-{desktop:"../src/assets/images/desktop/image-pocket-borealis.jpg",
-mobile:"../src/assets/images/mobile/image-pocket-borealis.jpg",
+{desktop:pocketborealisDesktop,
+mobile:pocketborealismobile,
 text:'POCKET',
 texts:'BOREALIS',
 },
-{desktop:"../src/assets/images/desktop/image-curiosity.jpg",
- mobile:"../src/assets/images/mobile/image-curiosity.jpg",
+{desktop:curiosityDesktop,
+ mobile:curiositymobile,
  text:'THE',
  texts:'CURIOSITY',
  },
  {
-  desktop:"../src/assets/images/desktop/image-fisheye.jpg",
-  mobile:"../src/assets/images/mobile/image-fisheye.jpg",
+  desktop:fisheyeDesktop,
+  mobile:fisheyemobile,
   text:'MAKE IT',
   texts:'FISHEYE',
  }
@@ -78,7 +93,7 @@ export default function Gallery() {
             mobileSrc={Galleryimages.mobile}
             text={Galleryimages.text}
             texts={Galleryimages.texts}
-            alt={`Image ${index}`}
+
           />
         ))}
       </div>
